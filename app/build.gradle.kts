528364382f4b22d6abc3cdfe343e0f0b46dbbdc3
Daniele De Francesco <github@qq7te.com>
2025-08-10 15:05:01 -0400

    new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.qq7te.totalrecall"
         minSdk = 31
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
