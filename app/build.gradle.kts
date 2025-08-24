3ed5dbb0ace3912e420884e62101b998e5e97af1
Daniele De Francesco <github@qq7te.com>
2025-08-24 14:07:58 -0400

    Update version to 0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.qq7te.totalrecall"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
